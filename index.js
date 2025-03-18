aacd789b571f42494b4217966787cc4cc8ca27be
zaybe <zaybe.maliq@student.moringaschool.com>
2025-03-18 11:09:54 +0300

    add new files

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression called divide
+const divide = function(a, b) {
+    return a / b;
+};
+
+// Arrow function called square
+const square = (x) => {
+    return x * x;
+};
+
+// Arrow function called add
+const add = (a, b) => {
+    return a + b;
+};
